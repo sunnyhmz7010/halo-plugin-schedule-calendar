a6cc998aadff85591713420d83c1b741932d2766
sunnyhmz7010 <52040067+sunnyhmz7010@users.noreply.github.com>
2026-03-22 13:46:32 +0800

    Register schedule entry scheme on plugin startup

--- src/main/java/run/halo/schedule/calendar/ScheduleCalendarPlugin.java
+++ src/main/java/run/halo/schedule/calendar/ScheduleCalendarPlugin.java
@@ -1,13 +1,26 @@
 package run.halo.schedule.calendar;
 
 import org.springframework.stereotype.Component;
+import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.BasePlugin;
 import run.halo.app.plugin.PluginContext;
 
 @Component
 public class ScheduleCalendarPlugin extends BasePlugin {
+    private final SchemeManager schemeManager;
 
-    public ScheduleCalendarPlugin(PluginContext pluginContext) {
+    public ScheduleCalendarPlugin(PluginContext pluginContext, SchemeManager schemeManager) {
         super(pluginContext);
+        this.schemeManager = schemeManager;
+    }
+
+    @Override
+    public void start() {
+        schemeManager.register(ScheduleEntry.class);
+    }
+
+    @Override
+    public void stop() {
+        schemeManager.unregister(ScheduleEntry.class);
     }
 }
